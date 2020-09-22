9d69744988d8d1daf47cc002a8e72a2042edfbbe
nicoAlvarezMern <nicolas.alvarez.mern@gmail.com>
2020-09-22 14:50:40 -0300

    ss

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -30,12 +30,12 @@ export function AppRouter() {
             <Navbar toggleDrawer={toggleDrawer}/>
             <TemporaryDrawer toggleDrawer={toggleDrawer} state={state}/>
             <Switch>
-                <Route exact path="/resumen" component={Resume} />
+                <Route exact path="./resumen" component={Resume} />
                 <Route exact path="/sobremi" component={AboutMe} />
                 <Route exact path="/proyectos" component={Projects} />
                 <Route exact path="/contacto" component={Contact} />
                 <Route path="/" component={PortFolio} />
-                <Redirect to="/resumen" />
+                <Redirect to="/" />
             </Switch>
         </div>
     </Router>
